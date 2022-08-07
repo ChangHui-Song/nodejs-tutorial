c763e83cafd72c335f4802a96c867a08317a14b1
sch <73852281+ChangHui-Song@users.noreply.github.com>
2022-08-07 17:11:38 +0900

    update module example

---
+++ nodejs-textbook/node-feature-tutorial/module-example/es2015/func.mjs
@@ -0,0 +1,5 @@
+import number from './var.mjs';
+
+console.log(number);
+// console.log(odd);
+// console.log(even);
---
+++ nodejs-textbook/node-feature-tutorial/module-example/es2015/var.mjs
@@ -0,0 +1,4 @@
+const odd = 'odd';
+const even = 'even';
+
+export default { odd, even };
