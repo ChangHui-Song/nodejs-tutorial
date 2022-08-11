d4894239f9789069bfa71271f30db8f13a0179c8
sch <73852281+ChangHui-Song@users.noreply.github.com>
2022-08-11 22:23:03 +0900

    template engine example

---
+++ nodejs-textbook/express-example/template-engine/nunjucks/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const nunjucks = require('nunjucks');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use('/', express.static(__dirname));
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'njk');
+
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+
+app.get('/', (req, res) => {
+  res.locals.title = 'Express';
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>Hello, About<h1>');
+});
+
+app.listen(app.get('port'));
