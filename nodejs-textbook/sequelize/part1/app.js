8a124b2a90f1eeecc87c3a89772350ed342b7078
sch <73852281+ChangHui-Song@users.noreply.github.com>
2022-08-13 22:54:04 +0900

    sequelize part 1 example

---
+++ nodejs-textbook/sequelize/part1/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const { sequelize } = require('./models');
+const { nextTick } = require('process');
+
+const app = express();
+app.set(app.get(process.env.PORT) || 3000);
+app.set('view engine', 'pug');
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log('connect success!');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  const error = new Error(`not found ${req.method} ${req.url}`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`listening ${app.get('port')}`);
+});
