e2f83186ba07ced318ba6f6968b436c5dde05df3
ChangHui-Song <sch102403@gmail.com>
2022-08-23 10:17:42 +0900

    카카오 로그인 구현

--- nodejs-textbook/nodebird/routes/auth.js
+++ nodejs-textbook/nodebird/routes/auth.js
@@ -46,9 +46,26 @@ router.post('/login', isNotLoggedIn, (req, res, next) => {
 });
 
 router.get('/logout', isLoggedIn, (req, res) => {
-  req.logout();
-  req.session.destroy();
-  res.redirect('/');
+  req.logout((err) => {
+    req.session.destroy();
+    if (err) {
+      res.redirect('/');
+    } else {
+      res.status(200).redirect('/');
+    }
+  });
 });
+
+router.get('/kakao', passport.authenticate('kakao'));
+
+router.get(
+  '/kakao/callback',
+  passport.authenticate('kakao', {
+    failureRedirect: '/',
+  }),
+  (req, res) => {
+    res.redirect('/');
+  }
+);
 
 module.exports = router;
