7dd39ae0f57194220e495a93cf070446a06b0ac2
ChangHui-Song <sch102403@gmail.com>
2022-08-28 10:18:40 +0900

    팔로우 해제 기능 추가

--- nodejs-textbook/nodebird/routes/user.js
+++ nodejs-textbook/nodebird/routes/user.js
@@ -19,4 +19,19 @@ router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
   }
 });
 
+router.post('/:nick/unfollow', isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { nick: req.params.nick } });
+    console.log(user);
+    if (!user) {
+      res.status(404).send('no user');
+    }
+    await user.removeFollower(parseInt(req.user.id, 10));
+    res.send('success');
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 module.exports = router;
