c50ed69d574f1be1259f4febdb8c3fbab706cfb2
ChangHui-Song <sch102403@gmail.com>
2022-09-05 14:42:24 +0900

    JWT 토큰 인증 구현

--- nodejs-textbook/nodebird-api/routes/middleware.js
+++ nodejs-textbook/nodebird-api/routes/middleware.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const jwt = require('jsonwebtoken');
+
 exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
     return res.status(403).send('not logged in');
@@ -13,4 +15,22 @@ exports.isNotLoggedIn = (req, res, next) => {
     return res.redirect(`/?error${message}`);
   }
   next();
+};
+
+exports.verifyToken = (req, res, next) => {
+  try {
+    req.decoded = jwt.verify(req.headers.authoriztion, process.env.JWT_SECRET);
+    return next();
+  } catch (error) {
+    if (error.name === 'TokenExpiredError') {
+      return res.status(419).json({
+        code: 419,
+        message: '토큰이 만료되었습니다.',
+      });
+    }
+    return res.status(401).json({
+      code: 401,
+      message: '유효하지 않은 토큰입니다.',
+    });
+  }
 };
