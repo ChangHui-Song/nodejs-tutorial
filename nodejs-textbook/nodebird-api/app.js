8d67f5bae38c5f8c3294616fe68ba068587168f6
ChangHui-Song <sch102403@gmail.com>
2022-09-13 13:25:22 +0900

    api server 기본 구조

---
+++ nodejs-textbook/nodebird-api/app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const morgan = require('morgan');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const authRouter = require('./routes/auth');
+const indexRouter = require('./routes');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
+
+const app = express();
+passportConfig();
+app.set('port', process.env.PORT || 8002);
+app.set('view engine', 'html');
+nunjucks.config('views', {
+  express: app,
+  watch: true,
+});
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log('database connecting');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', authRouter);
+app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} not found`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.rocals.message = err.message;
+  res.rocals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`open port ${app.get('port')}`);
+});
