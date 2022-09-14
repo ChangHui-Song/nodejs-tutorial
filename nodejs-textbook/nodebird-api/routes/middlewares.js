0e29954c831e38fb48e54fd05c600a9303be0141
ChangHui-Song <sch102403@gmail.com>
2022-09-14 16:32:10 +0900

    api 호출량 제한 구현 및 테스트

--- nodejs-textbook/nodebird-api/routes/middlewares.js
+++ nodejs-textbook/nodebird-api/routes/middlewares.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const jwt = require('jsonwebtoken');
+const RateLimit = require('express-rate-limit');
 
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -35,4 +36,22 @@ exports.verifyToken = (req, res, next) => {
       message: '유효하지 않은 토큰입니다.',
     });
   }
+};
+
+exports.apiLimiter = RateLimit({
+  windowMs: 60 * 1000,
+  max: 10,
+  handler(req, res) {
+    res.status(this.statusCode).json({
+      code: this.status,
+      message: '1분에 한 번만 요청 할 수 있습니다.',
+    });
+  },
+});
+
+exports.deprecated = (req, res) => {
+  res.status(410).json({
+    code: 410,
+    message: '새로운 버전이 나왔습니다. 새로운 버전을 사용하세요.',
+  });
 };
--- nodejs-textbook/nodebird-api/routes/v2.js
+++ nodejs-textbook/nodebird-api/routes/v2.js
@@ -1,29 +1,25 @@
-'use strict';
-
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-const { verifyToken, deprecated } = require('./middlewares');
+const { verifyToken, apiLimiter } = require('./middlewares');
 const { Domain, User, Post, Hashtag } = require('../models');
 
 const router = express.Router();
-router.use(deprecated);
 
 router.post('/token', async (req, res) => {
   const { clientSecret } = req.body;
-  console.log(clientSecret);
   try {
     const domain = await Domain.findOne({
       where: { clientSecret },
       include: {
         model: User,
-        attribute: ['nick', 'id'],
+        attributes: ['nick', 'id'],
       },
     });
     if (!domain) {
       return res.status(401).json({
         code: 401,
-        message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
+        message: '등록되지 않은 도메인 입니다.',
       });
     }
     const token = jwt.sign(
@@ -33,7 +29,7 @@ router.post('/token', async (req, res) => {
       },
       process.env.JWT_SECRET,
       {
-        expiresIn: '1m',
+        expiresIn: '30m',
         issuer: 'nodebird',
       }
     );
@@ -51,11 +47,11 @@ router.post('/token', async (req, res) => {
   }
 });
 
-router.get('/test', verifyToken, (req, res) => {
+router.get('/test', verifyToken, apiLimiter, (req, res) => {
   res.json(req.decoded);
 });
 
-router.get('/posts/my', verifyToken, (req, res) => {
+router.get('/posts/my', verifyToken, apiLimiter, async (req, res) => {
   Post.findAll({ where: { userId: req.decoded.id } })
     .then((posts) => {
       console.log(posts);
@@ -68,34 +64,39 @@ router.get('/posts/my', verifyToken, (req, res) => {
       console.error(error);
       return res.status(500).json({
         code: 500,
-        message: 'server error',
+        message: '서버 에러',
       });
     });
 });
 
-router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
-  try {
-    const hashtag = await Hashtag.findOne({
-      where: { title: req.params.title },
-    });
-    if (!hashtag) {
-      return res.status(404).json({
-        code: 404,
-        message: '검색 결과가 없습니다.',
+router.get(
+  '/posts/hashtag/:title',
+  verifyToken,
+  apiLimiter,
+  async (req, res) => {
+    try {
+      const hashtag = await Hashtag.findOne({
+        where: { title: req.params.title },
+      });
+      if (!hashtag) {
+        return res.status(404).json({
+          code: 404,
+          message: '검색 결과가 없습니다.',
+        });
+      }
+      const posts = hashtag.getPosts();
+      return res.json({
+        code: 200,
+        payload: posts,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        code: 500,
+        message: '서버 에러',
       });
     }
-    const posts = await hashtag.getPosts();
-    return res.json({
-      code: 200,
-      payload: posts,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({
-      code: 500,
-      message: 'server error',
-    });
   }
-});
+);
 
 module.exports = router;
