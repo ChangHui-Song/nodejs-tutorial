b78fa6acafed28b96bb3010c550d812bfa70c129
ChangHui-Song <sch102403@gmail.com>
2022-09-20 15:05:14 +0900

    addFollowing controller 분리 후 테스팅

--- nodejs-textbook/nodebird/routes/user.js
+++ nodejs-textbook/nodebird/routes/user.js
@@ -2,22 +2,11 @@ const express = require('express');
 
 const { isLoggedIn } = require('./middlewares');
 const User = require('../models/user');
+const { addFollowing } = require('../controllers/user');
 
 const router = express.Router();
 
-router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
-  try {
-    const user = await User.findOne({ where: { id: req.user.id } });
-    if (!user) {
-      res.status(404).send('no user');
-    }
-    await user.addFollowing(parseInt(req.params.id, 10));
-    res.send('success');
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
+router.post('/:id/follow', isLoggedIn, addFollowing);
 
 router.post('/:nick/unfollow', isLoggedIn, async (req, res, next) => {
   try {
