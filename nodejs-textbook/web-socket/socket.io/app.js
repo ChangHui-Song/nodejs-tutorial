ad9e7e32d33f9d70923ab1b44427cc9a2424f080
ChangHui-Song <sch102403@gmail.com>
2022-09-23 13:15:51 +0900

    채팅 웹 소켓 구현

--- nodejs-textbook/web-socket/socket.io/app.js
+++ nodejs-textbook/web-socket/socket.io/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
+const ColorHash = require('color-hash').default;
 
 dotenv.config();
 const webSocket = require('./socket');
@@ -20,22 +21,30 @@ nunjucks.configure('views', {
 });
 connect();
 
+const sessionMiddleware = session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+});
+
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-    },
-  })
-);
+app.use(sessionMiddleware);
+
+app.use((req, res, next) => {
+  if (!req.session.color) {
+    const colorHash = new ColorHash();
+    req.session.color = colorHash.hex(req.sessionID);
+  }
+  next();
+});
 
 app.use('/', indexRouter);
 
@@ -56,4 +65,4 @@ const server = app.listen(app.get('port'), () => {
   console.log(`${app.get('port')} connected`);
 });
 
-webSocket(server);
+webSocket(server, app, sessionMiddleware);
