35b6ea9e9c6204b5e82c1541252caf15db52d658
sch <sch102403@gmail.com>
2023-02-08 15:57:16 +0900

    파일 업로드 기능 연습

--- codecamp/image-upload/backend/src/file/file.service.ts
+++ codecamp/image-upload/backend/src/file/file.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { Storage } from '@google-cloud/storage';
 
 @Injectable()
 export class FileService {
-  upload({ files }) {
-    console.log(files);
-    // 구글 스토리지에 파일 업로드하기
-    return ['~~~url'];
+  async upload({ files }) {
+    const waitedFiles = await Promise.all(files);
+
+    const storage = new Storage({
+      projectId: process.env.GOOGLE_PROJECTID,
+      keyFilename: process.env.GOOGLE_KEYFILE,
+    }).bucket(process.env.GOOGLE_BUCKET);
+
+    const promisedFiles = waitedFiles.map((file) => {
+      return new Promise((resolve, reject) => {
+        file
+          .createReadStream()
+          .pipe(storage.file(file.filename).createWriteStream())
+          .on('finish', () =>
+            resolve(`${process.env.GOOGLE_BUCKET}/${file.filename}`),
+          )
+          .on('error', () => reject('fail'));
+      });
+    });
+    const result = await Promise.all(promisedFiles);
+
+    return result;
   }
 }
