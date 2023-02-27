f2312982d41c356146980a59089a7a6095d6a848
sch <sch102403@gmail.com>
2023-02-27 23:21:38 +0900

    unit test version 2

---
+++ codecamp/test/src/unit.v2.spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const appModule: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = appModule.get<AppController>(AppController);
+  });
+
+  describe('getHello', () => {
+    it('hello world', () => {
+      const result = appController.getHello();
+      expect(result).toBe('Hello World!');
+    });
+  });
+});
